3a16ed00a59689cca8099434053863299bcd08b9
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2025-04-17 17:05:57 +0530

    Add files via upload

---
+++ rangeodd.py
@@ -0,0 +1,10 @@
+userInput = input()
+L,R=userInput.split()
+sum=0
+print("The Input Provided is: " + L,R)
+for i in range(int(L),int(R)):
+    if i%2==0:
+        None
+    else:
+        sum=sum+i    
+print(sum)        
